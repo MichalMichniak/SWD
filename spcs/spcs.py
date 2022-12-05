bf8570abc3aebe5547aea29a20c3080133d7665f
JakubPac <jakubpacon@student.agh.edu.pl>
2022-12-05 15:42:29 +0100

    ss

---
+++ spcs/spcs.py
@@ -0,0 +1,6 @@
+from typing import List,Callable
+from zdominowane import zdominowane
+import numpy as np
+
+def SPCS(A : List[List[float]], C :  List[List[float]],min_max_criterial_funct : List[Callable[[np.ndarray],float]],metr = None):
+    pass
