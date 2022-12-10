75554e3d503bf4832017a898f8f0376a1462f977
MichalMichniak <michalmichniak2001@gmail.com>
2022-12-10 15:54:51 +0100

    woronoj

--- spcs/woronoj_pkt.py
+++ spcs/woronoj_pkt.py
@@ -1,9 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def woronoj(pkt_1 : np.ndarray,pkt_2 : np.ndarray):
     N = len(pkt_1)
+    woronoj_points = [0 for i in range(2*N)]
+    woronoj_points[0] = pkt_1
+    woronoj_points[-1] = pkt_2
     size_of_shift = np.abs(pkt_2 - pkt_1)/2
-    size_of_shift = np.abs(pkt_2 - pkt_1)/2
-    axis = [True for i in range(N)]
-    for i in range(N):
+    sign_of_shift = np.sign(pkt_2 - pkt_1)
+    mask_fixed = np.zeros(N)
+    for i in range(1,N):
+        woronoj_points[i] = woronoj_points[0] + np.min(size_of_shift) * sign_of_shift + mask_fixed
+        woronoj_points[2*N-1-i] = woronoj_points[-1] + -1*np.min(size_of_shift) * sign_of_shift - mask_fixed
+        idx = np.argmin(size_of_shift)
+        mask_fixed[idx] = woronoj_points[i][idx]
+        size_of_shift[idx] = np.inf
+        sign_of_shift[idx] = 0
+    return np.array(woronoj_points)
 
+if __name__ == '__main__':
+    x1 = np.array([0,0,0])
+    x2 = np.array([2,4,8])
+    wor = woronoj(x1,x2)
+
+
+    fig = plt.figure()
+    ax = plt.axes(projection ='3d')
+    ax.plot3D(wor[:,0], wor[:,1], wor[:,2], 'green')
+    plt.show()
