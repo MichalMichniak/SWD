ec6b5e520f03a4efa1c0279ff3dc048f7e7b7779
MichalMichniak <81298429+MichalMichniak@users.noreply.github.com>
2022-12-17 16:59:21 +0100

    Add files via upload

--- metody_typu_UTA/funkcja_uzytecznosci.py
+++ metody_typu_UTA/funkcja_uzytecznosci.py
@@ -2,10 +2,11 @@ import numpy as np
 
 def zwroc_f_u(y_u_params, x_u_params):
     f = []
+
     for i in range(len(y_u_params)):
         a_b = []
-        for j in range(len(y_u_params[i][1:])):
-            a = (y_u_params[i][j-1]-y_u_params[i][j])/(x_u_params[i][j-1]-x_u_params[i][j])
-            a_b.append([a,y_u_params[i][j-1]-a*x_u_params[i][j-1]])
+        for j in range(1,len(y_u_params[i])):
+            a = (y_u_params[i][::-1][j-1]-y_u_params[i][::-1][j])/(x_u_params[i][j-1]-x_u_params[i][j])
+            a_b.append([a,y_u_params[i][::-1][j-1]-a*x_u_params[i][j-1]])
         f.append(a_b)
     return f
